a2eac099cf062739e865ad90979022414546ecc5
hodi14 <alirezahodaee14@gmail.com>
2021-03-14 11:01:41 +0330

    addng category section to upload page

--- public/assets/js/uploadPage.js
+++ public/assets/js/uploadPage.js
@@ -1,3 +1,4 @@
+/* Pricing */
 $(".pricing label.free").click(function () {
     $(this).addClass("selected");
     $(".pricing label.toman").removeClass("shown");
@@ -5,4 +6,13 @@ $(".pricing label.free").click(function () {
 $(".pricing input").click(function () {
     $(".pricing label.free").removeClass("selected");
     $(".pricing label.toman").addClass("shown");
+});
+
+/* Category */
+$(".category .options label.radioLabel").click(function () {
+    var labels = $(".category .options label.radioLabel");
+    for (var i = 0; i < 3; i++) {
+        $(labels[i]).removeClass("selected");
+    }
+    $(this).addClass("selected");
 });
