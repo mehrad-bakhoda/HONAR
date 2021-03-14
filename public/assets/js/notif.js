4120f6caaa25849577ebb7d71260ea514db1aba6
Erfan Ramezani <erfanrmz@gmail.com>
2021-03-14 14:16:35 +0330

    email and phoneNumber validation

--- public/assets/js/notif.js
+++ public/assets/js/notif.js
@@ -1,4 +1,5 @@
 function placeholderError(input, message) {
+    $(input).val()
     $(input).attr({ "placeholder": message });
     $(input + "::placeholder").css({ "color": "#d14774" });
 }
@@ -6,8 +7,36 @@ function placeholderError(input, message) {
 function notifError(message) {
     $(".notif.error p").html(message);
     $(".notif.error").addClass("showNotif");
+    setTimeout(function() {
+        $(".notif.error").removeClass("showNotif");
+    }, 4000);
 }
 function notifSucces(message) {
     $(".notif.succes p").html(message);
     $(".notif.success").addClass("showNotif");
+    setTimeout(function() {
+        $(".notif.success").removeClass("showNotif");
+    }, 4000);
 }
+
+function validateEmail(input) {
+    const email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const phone = /^[0-9]{11}$/;
+    return email.test(String(input).toLowerCase()) || phone.test(input)
+}
+
+
+//login notif
+$(".next")
+.on('click', function(e) {
+    var input = $(".phoneInput").val();
+    $(".phoneInput").val(input);
+    if (!validateEmail(input))
+    {
+        placeholderError(".phoneInput","pass is wrong");
+        notifError("لطفا ایمیل یا شماره تلفن معتبر وارد نمایید")
+        e.preventDefault();
+
+    }
+    
+});
