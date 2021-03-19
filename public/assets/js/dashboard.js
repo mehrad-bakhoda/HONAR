1702ae7315ad8d04b853bc51fac962f7555ca714
hodi14 <alirezahodaee14@gmail.com>
2021-03-19 14:11:43 +0330

    dashboard personal info

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,5 +1,21 @@
+/* Collapse */
 function openContent(className) {
     console.log(className);
-    $("."+ className+ ".part .collapseContent").toggleClass("contentHidden");
-    $("."+ className + ".part i.fa-angle-down").toggleClass("rotated");
+    $("." + className + ".part .collapseContent").toggleClass("contentHidden");
+    $("." + className + ".part i.fa-angle-down").toggleClass("rotated");
 }
+
+/* Edit Info */
+$(".personalInfo input").attr({'disabled': 'disabled' });
+$(".personalInfo textarea").attr({ 'disabled': 'disabled' });
+function changeForm() {
+    $(".personalInfo .buttons button").toggleClass("hiddenButton");
+}
+$(".personalInfo .buttons .edit").click(function () {
+    $(".personalInfo input").removeAttr("disabled");
+    $(".personalInfo textarea").removeAttr("disabled");
+});
+$(".personalInfo .buttons .after").click(function () {
+    $(".personalInfo input").attr({ 'disabled': 'disabled' });
+    $(".personalInfo textarea").attr({ 'disabled': 'disabled' });
+});
