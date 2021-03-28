5f52daa061a696edfe4a440d7dfdc38b2fee88f2
hodi14 <alirezahodaee14@gmail.com>
2021-03-29 00:35:59 +0430

    adding loader for file uploads v1.0

---
+++ public/assets/js/uploadLoader.js
@@ -0,0 +1,4 @@
+$(".loginPage .tab-pane .uploadPic input").click(function () {
+    $(".loginPage .tab-pane .uploadPic i.fa-spinner-third").removeClass("hidden");
+    $(".loginPage .tab-pane .uploadPic i.fa-spinner-third").addClass("fa-spin");
+})
