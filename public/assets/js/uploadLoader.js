7fccef04057b8a475b78716e14b196e47a9f335f
hodi14 <alirezahodaee14@gmail.com>
2021-03-29 01:07:22 +0430

    fixing file loader icon

--- public/assets/js/uploadLoader.js
+++ public/assets/js/uploadLoader.js
@@ -1,4 +1,3 @@
 $(".loginPage .tab-pane .uploadPic input").click(function () {
     $(".loginPage .tab-pane .uploadPic i.fa-spinner-third").removeClass("hidden");
-    $(".loginPage .tab-pane .uploadPic i.fa-spinner-third").addClass("fa-spin");
 })
