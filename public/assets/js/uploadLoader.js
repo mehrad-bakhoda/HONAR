31d40c2649a7a0add4311267198f6c043e9476c3
Erfan Ramezani <erfanrmz@gmail.com>
2021-04-01 12:55:21 +0430

    pullin request

--- public/assets/js/uploadLoader.js
+++ public/assets/js/uploadLoader.js
@@ -14,3 +14,16 @@ $("#fileUploader").change(event =>{
         }
     })
 })
+$("#productUploader").change(event =>{
+    $(".uploadPage .uploadPic i.fa-spinner-third").removeClass("hidden");
+    const files = event.target.files;
+    const file = files[0];
+    reader.readAsDataURL(file);
+    reader.addEventListener('progress',event =>{
+        if(event.loaded == event.total)
+        {
+            $(".uploadPage .uploadPic i.fa-spinner-third").addClass("hidden");
+            $(".uploadPage .uploadPic i.fa-check-circle").removeClass("hidden");
+        }
+    })
+})
