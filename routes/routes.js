51fa2f55be74dbeaaef3720daa1875ea5df6e14c
Mehrad Bakhoda <no2.nemesis@gmail.com>
2021-05-07 20:39:18 +0430

    Cannot set headers warning fixed

--- routes/routes.js
+++ routes/routes.js
@@ -76,8 +76,9 @@ router.get("/login",function(req,res){
   if (req.session.userId)
   {
     res.redirect("/dashboard")
-  }
+  }else{
   res.render("login",{ inputFouned:false,inputVerify:true,loginInput:req.body.loginInput,newUser:false,back:true});
+}
   });
 
 
