538f7563484eb672dc146f4b3a96d72e0766031a
hodi14 <alirezahodaee14@gmail.com>
2021-05-07 22:43:31 +0430

    style fixes

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -3,6 +3,7 @@ function openContent(className) {
     console.log(className);
     $("." + className + ".part .collapseContent").toggleClass("contentHidden");
     $("." + className + ".part i.fa-angle-down").toggleClass("rotated");
+    $("." + className).toggleClass("opened");
 }
 
 /* Edit Info */
