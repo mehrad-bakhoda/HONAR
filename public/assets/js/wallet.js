f4ef3a49c6ea1c5fff74efd050a0db143ed5aeb3
Hodi14 <alirezahodaee14@gmail.com>
2021-05-30 20:02:08 +0430

    dashboard wallet

--- public/assets/js/wallet.js
+++ public/assets/js/wallet.js
@@ -1,3 +1,3 @@
-$(".dashboardPage #walletModal .cash p.add").click(function () {
-    $(".dashboardPage #walletModal .carts").toggleClass("hidden");
+$(".dashboardPage .wallet .cash p.add").click(function () {
+    $(".dashboardPage .wallet .carts").toggleClass("hidden");
 })
