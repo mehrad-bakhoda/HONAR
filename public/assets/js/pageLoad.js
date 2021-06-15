31ede5798625a4bf1de4cfb671e040672c39c922
erfanrmz <erfanrmz@gmail.com>
2021-06-15 13:37:25 +0430

    right click and taphold in phones disabled

--- public/assets/js/pageLoad.js
+++ public/assets/js/pageLoad.js
@@ -2,3 +2,10 @@ $(document).ready(function () {
     $(".page").fadeIn(1000);
     $(".uploadButton").fadeIn(1000);
 })
+$("img").on("taphold", function(event) {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+ })
+ $('img').bind("contextmenu",function(e){
+    return false;
+});
