9789dc12cbbe7d2572d864e0e7f2f696a9b10258
Hodi14 <alirezahodaee14@gmail.com>
2021-10-15 09:03:05 +0330

    fixed product page custom selects

--- public/assets/js/chooseSize.js
+++ public/assets/js/chooseSize.js
@@ -1,88 +1,36 @@
-var addToCartButton = $(".addToCart");
-var buyButton = $(".buy");
-var downloadButton = $(".download");
-var bought = $("#bought");
-var price1 = $(".infos .price p." + "original").children("span").text();
-if(price1 == "" || "original" == bought.val())
-    {
-        downloadButton.removeAttr('hidden');
-        addToCartButton.attr("hidden",true);
-        buyButton.attr("hidden",true);
-    }
-else{
-    downloadButton.attr("hidden",true);
-    addToCartButton.removeAttr('hidden');
-    buyButton.removeAttr('hidden');
+const filesArrow = document.querySelector(".fileTypes .shownType i");
+const fileOptions = document.querySelector(".fileTypes .options");
+const fOptions = document.querySelectorAll(".fileTypes .fileOption");
+filesArrow.onclick = () => {
+    fileOptions.classList.toggle("opened");
+    filesArrow.classList.toggle("rotated");
+
 }
-$(".infos .sizes label.sizeOption").click(function () {
-    var sizes = $(".infos .sizes label.sizeOption");
-    $(".infos .sizes label.selectedSize").text($(this).text());
+for (let fOption of fOptions) {
+    fOption.onclick = () => {
+        document.querySelector(".fileOptions .shownType label").innerText = fOption.innerText;
+        fOption.classList.add("selected");
+        fileOptions.classList.toggle("opened");
+        filesArrow.classList.toggle("rotated");
 
-    for (var i = 0; i < sizes.length; i++) {
-        $(sizes[i]).removeClass("selected");
     }
-    $(this).addClass("selected");
+}
 
-    var prices = $(".infos .price p");
-    var currentID = $(this).attr("id");
-    var currentPrice = $(".infos .price p." + currentID);
-    for (var i = 0; i < prices.length; i++) {
-        $(prices[i]).removeClass("showPrice");
+
+const sizeArrow = document.querySelector(".sizes .shownSize i");
+const sizeOptions = document.querySelector(".sizes .options");
+const sOptions = document.querySelectorAll(".sizes .sizeOption");
+
+sizeArrow.onclick = () => {
+    sizeOptions.classList.toggle("opened");
+    sizeArrow.classList.toggle("rotated");
+}
+for (let sOption of sOptions) {
+    sOption.onclick = () => {
+        document.querySelector(".sizes .shownSize label").innerText = sOption.innerText;
+        sOption.classList.add("selected");
+        sizeOptions.classList.toggle("opened");
+        sizeArrow.classList.toggle("rotated");
+
     }
-
-    $(".infos .sizes .options").toggleClass("opened");
-    $(".infos .sizes label.sizeOption").toggleClass("show");
-    $(".infos .sizes i").toggleClass("rotated");
-
-    currentPrice.addClass("showPrice");
-    var price = currentPrice.children("span").text();
-    console.log(bought.val());
-    if(price == "" || currentID == bought.val())
-    {
-        downloadButton.removeAttr('hidden');
-        addToCartButton.attr("hidden",true);
-        buyButton.attr("hidden",true);
-    }
-    else{
-        downloadButton.attr("hidden",true);
-        addToCartButton.removeAttr('hidden');
-        buyButton.removeAttr('hidden');
-    }
- 
-        
-    var href = $('#addToCart').attr('href');
-    var href1 = $('#downloadButton').attr('href');
-    $('#addToCart').attr('href',href.replace(href.split("/")[3],currentID))
-    $('#downloadButton').attr('href',href1.replace(href1.split("/")[3],currentID))
-    
-})
-
-$(".infos .sizes .shownSize").click(function () {
-    $(".infos .sizes .options").toggleClass("opened");
-    $(".infos .sizes label.sizeOption").toggleClass("show");
-    $(".infos .sizes i").toggleClass("rotated");
-});
-
-
-//////////////////////////////////////////// File Types ////////////////////////////////////////////
-$(".infos .fileTypes label.typeOption").click(function () {
-    var types = $(".infos .fileTypes label.typeOption");
-    $(".infos .fileTypes label.selectedType").text($(this).text());
-
-    for (var i = 0; i < types.length; i++) {
-        $(types[i]).removeClass("selected");
-    }
-    $(this).toggleClass("selected");
-
-
-    $(".infos .fileTypes .options").toggleClass("opened");
-    $(".infos .fileTypes label.typeOption").toggleClass("show");
-    $(".infos .fileTypes i").toggleClass("rotated");
-
-})
-
-$(".infos .fileTypes .shownType").click(function () {
-    $(".infos .fileTypes .options").toggleClass("opened");
-    $(".infos .fileTypes label.typeOption").toggleClass("show");
-    $(".infos .fileTypes i").toggleClass("rotated");
-});
+}
